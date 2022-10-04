6744921618a081b3f96fb208e39ffbeb2ada45df
mwaraem <mwaraem@gmail.com>
2022-10-04 14:17:27 +0300

    Final Commit

--- app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
+++ app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
@@ -8,10 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
-import androidx.preference.EditTextPreference
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
+import androidx.preference.*
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -19,11 +16,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
 
+        val dataStore = DataStore()
+
+        preferenceManager.preferenceDataStore = dataStore
+
         val accSettingsPref = findPreference<Preference>(getString(R.string.key_account_settings))
 
         accSettingsPref?.setOnPreferenceClickListener {
 
-            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+            val navHostFragment =
+                activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
             val navController = navHostFragment.navController
             val action = SettingsFragmentDirections.actionSettingsToAccSettings()
             navController.navigate(action)
@@ -36,9 +38,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
         // Step 2: Get the 'value' using the 'key'
         val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time), "")
-        Log.i("SettingsFragment", "Auto Reply Time: $autoReplyTime")
 
-        val autoDownload = sharedPreferences.getBoolean(getString(R.string.key_auto_download), false)
+        val autoDownload =
+            sharedPreferences.getBoolean(getString(R.string.key_auto_download), false)
         Log.i("SettingsFragment", "Auto Download: $autoDownload")
 
         val statusPref = findPreference<EditTextPreference>("key_status")
@@ -46,13 +48,49 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
             val newStatus = newValue as String
             if (newStatus.contains("bad")) {
-                Toast.makeText(context, "Inappropriate Status. Pleas maintain community guidlines." ,
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    context, "Inappropriate Status. Pleas maintain community guidlines.",
+                    Toast.LENGTH_SHORT
+                ).show()
 
                 false
             } else {
                 true
             }
         }
+
+        val notificationPref = findPreference<SwitchPreferenceCompat>("key_new_msg_notif")
+        notificationPref?.summaryProvider =
+            Preference.SummaryProvider<SwitchPreferenceCompat> { switchPref ->
+
+                if (switchPref?.isChecked!!)
+                    "Status: ON"
+                else
+                    "Status: OFF"
+            }
+
+        notificationPref?.preferenceDataStore = dataStore
+    }
+
+    class DataStore : PreferenceDataStore() {
+        override fun getBoolean(key: String?, defValue: Boolean): Boolean {
+            if (key == "key_new_msg_notif") {
+                // Retrieve value from cloud or local db
+                Log.i("DataStore", "getBoolean executed for $key")
+            }
+
+            return defValue
+        }
+
+        override fun putBoolean(key: String?, value: Boolean) {
+
+            if(key == "key_new_msg_notif") {
+                // Save value to cloud or local db
+                Log.i("DataStore", "putBoolean executed for $key with new value: $value")
+            }
+        }
     }
 }
+
+
+
